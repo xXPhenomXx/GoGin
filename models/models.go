30cdddc4c5e08cb52dcd529514702d160300e38e
Jassen Michaels <jassen@apheliondevelopment.com>
2019-02-05 19:19:22 -0600

    first commit

---
+++ models/models.go
@@ -0,0 +1,53 @@
+package models
+
+import (
+	"fmt"
+	"log"
+
+	"gogin/pkg/setting"
+	"time"
+)
+
+var db *gorm.DB
+
+type Model struct {
+	ID         int `json:"id"`
+	CreatedOn  int `json:"created_on"`
+	ModifiedOn int `json:"modified_on"`
+	DeletedOn  int `json:"deleted_on"`
+}
+
+func Setup() {
+	var err error
+	db, err = gorm.Open(setting.DatabaseSetting.Type, fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8&parseTime=True&loc=Local",
+		setting.DatabaseSetting.User,
+		setting.DatabaseSetting.Password,
+		setting.DatabaseSetting.Host,
+		setting.DatabaseSetting.Name))
+
+	if err != nil {
+		log.Fatalf("models.Setup err: %v", err)
+	}
+
+	gorm.DefaultTableNameHandler = func(db *gorm.DB, defaultTableName string) string {
+		return setting.DatabaseSetting.TablePrefix + defaultTableName
+	}
+
+	db.SingularTable(true)
+	db.Callback().Create().Replace("gorm:update_time_stamp", updateTimeStampForCreateCallback)
+	db.Callback().Update().Replace("gorm:update_time_stamp", updateTimeStampForUpdateCallback)
+	db.Callback().Delete().Replace("gorm:delete", deleteCallback)
+	db.DB().SetMaxIdleConns(10)
+	db.DB().SetMaxOpenConns(100)
+}
+
+func CloseDB() {
+	defer db.Close()
+}
+
+func addExtraSpaceIfExist(str string) string {
+	if str != "" {
+		return " " + str
+	}
+	return ""
+}
