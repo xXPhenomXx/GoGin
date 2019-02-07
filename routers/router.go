44ac18330b3c84ebf2e2174dadd4b76384913437
Jassen Michaels <jmichaels@moreycorp.com>
2019-02-07 12:57:11 -0600

    Updated user routes

--- routers/router.go
+++ routers/router.go
@@ -5,17 +5,17 @@ import (
 
 	"github.com/gin-gonic/gin"
 
-	_ "github.com/xxphenomxx/GoGin/docs"
 	"github.com/swaggo/gin-swagger"
 	"github.com/swaggo/gin-swagger/swaggerFiles"
+	_ "github.com/xxphenomxx/GoGin/docs"
 
-	"github.com/xxphenomxx/GoGin/middleware/jwt"
 	"github.com/xxphenomxx/GoGin/middleware/cors"
+	"github.com/xxphenomxx/GoGin/middleware/jwt"
 	"github.com/xxphenomxx/GoGin/pkg/export"
 	"github.com/xxphenomxx/GoGin/pkg/setting"
 	"github.com/xxphenomxx/GoGin/pkg/upload"
 	"github.com/xxphenomxx/GoGin/routers/api"
-	"github.com/xxphenomxx/GoGin/routers/api/v1"
+	//"github.com/xxphenomxx/GoGin/routers/api/v1"
 )
 
 func InitRouter() *gin.Engine {
@@ -35,11 +35,17 @@ func InitRouter() *gin.Engine {
 	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 	r.POST("/upload", api.UploadImage)
 
+	// unprotected user routes
+	users := r.Group("/user")
+	users.Use()
+	{
+		users.GET("/all", api.GetUsers)
+	}
+
+	// protected routes
 	apiv1 := r.Group("/api/v1")
 	apiv1.Use(jwt.JWT())
 	{
-		// user routes
-		apiv1.GET("/users", v1.GetUsers)
 
 
 	}
